02c03db58c1e8c5578006d5b2920fc5ca1dd14e6
Adam Shapiro <adam@pointonenav.com>
2025-05-28 10:19:57 -0400

    Reworked device partial name/UUID search for Balena SDK >= 13.x.

--- point_one/balena/device.py
+++ point_one/balena/device.py
@@ -4,7 +4,12 @@ import os
 import re
 import sys
 
-from balena.base_request import BaseRequest
+import balena
+try:
+    from balena.base_request import BaseRequest
+    have_base_request = True
+except:
+    have_base_request = False
 from balena.exceptions import DeviceNotFound
 
 # Relative imports don't usually work when running a Python file as a script since the file is not considered to be part
@@ -41,28 +46,50 @@ def get_device_uuid(name_or_uuid, is_name=None, return_name=False, balena=None, 
         except DeviceNotFound:
             pass
 
+    # List all devices, needed for partial name/UUID searches below. This is inefficient if there are a lot of devices,
+    # but is needed in more recent versions of the Balena SDK. See below for the explanation.
+    if not have_base_request:
+        all_devices = balena.models.device.get_all()
+
     # If this might be a device name, look now.
     devices_by_name = []
     if uuid is None and is_name is not False:
         __logger.debug("Trying device name query for '%s'." % name_or_uuid)
 
-        # Unfortunately, Balena's Python SDK does not have an efficient way to do a partial name match, but we can make
-        # a custom request manually. See https://forums.balena.io/t/python-sdk-partial-device-uuid-query/190973/33.
-        request = BaseRequest()
-        devices_by_name = request.request('device', 'GET',
-                                          raw_query="$filter=startswith(device_name, '%s')" % name_or_uuid,
-                                          endpoint=balena.settings.get('pine_endpoint'))['d']
-        for device in devices_by_name:
-            device['name'] = device['device_name']
+        # Balena's Python SDK does not have an efficient way to do a partial name match. Previously, the SDK allowed you
+        # to make a custom request and filter the data with a SQL-like query. This was suggested in
+        # https://forums.balena.io/t/python-sdk-partial-device-uuid-query/190973/33 back in 2020. In SDK ~10.x that
+        # looked like:
+        #   request = BaseRequest()
+        #   devices_by_name = request.request('device', 'GET',
+        #                                     raw_query="$filter=startswith(device_name, '%s')" % name_or_uuid,
+        #                                     endpoint=balena.settings.get('pine_endpoint'))['d']
+        #
+        # This API was removed in SDK 13.0.0 (the balena.base_request module doesn't exist at all anymore, nor does the
+        # BaseRequest class). Looking at the new SDK, in theory we should be able to do something similar by specifying
+        # a filter to get_all() like:
+        #   devices_by_name = balena.models.device.get_all({"$filter": f"startswith(device_name, '{name_or_uuid}')"})
+        # however, that just results in a 500 error from the server as of SDK 15.1.4.
+        #
+        # For now, the best we can do is query all devices with get_all() (called above), and then filter the result in
+        # Python. Calling get_all() can be a lot less efficient if the organization has a lot of devices, but there's
+        # not much we can do currently.
+        if not have_base_request:
+            devices_by_name = [d for d in all_devices if d['device_name'].startswith(name_or_uuid)]
+        else:
+            request = BaseRequest()
+            devices_by_name = request.request('device', 'GET',
+                                              raw_query="$filter=startswith(device_name, '%s')" % name_or_uuid,
+                                              endpoint=balena.settings.get('pine_endpoint'))['d']
 
         if len(devices_by_name) == 1:
             device = devices_by_name[0]
             if is_name:
                 uuid = device['uuid']
-                name = device['name']
+                name = device['device_name']
                 __logger.debug("Found device %s (%s) by name." % (name, uuid))
             else:
-                __logger.debug("Found candidate device %(name)s (%(uuid)s) by name." % device)
+                __logger.debug("Found candidate device %(device_name)s (%(uuid)s) by name." % device)
         elif len(devices_by_name) > 1:
             __logger.warning("Found multiple devices matching partial name string:\n    %s" %
                              "\n    ".join(["%(name)s (%(uuid)s)" % device for device in devices_by_name]))
@@ -70,9 +97,9 @@ def get_device_uuid(name_or_uuid, is_name=None, return_name=False, balena=None, 
             # If any of the devices is an exact match to the name, assume that's the correct match.
             if check_exact_match:
                 for device in devices_by_name:
-                    if device['name'] == name_or_uuid:
+                    if device['device_name'] == name_or_uuid:
                         uuid = device['uuid']
-                        name = device['name']
+                        name = device['device_name']
                         __logger.warning("Using exact match: %s (%s)." % (name, uuid))
 
     # If it's UUID-like and not explicitly a device name, see if it matches any known device.
@@ -80,26 +107,27 @@ def get_device_uuid(name_or_uuid, is_name=None, return_name=False, balena=None, 
     if uuid is None and not is_name and uuid_like and not full_uuid_length:
         __logger.debug("Trying partial UUID query for '%s'." % name_or_uuid)
 
-        # Unfortunately, Balena's Python SDK does not have an efficient way to do a partial UUID match, but we can make
-        # a custom request manually. See https://forums.balena.io/t/python-sdk-partial-device-uuid-query/190973/33.
-        request = BaseRequest()
-        devices_by_uuid = request.request('device', 'GET',
-                                          raw_query="$filter=startswith(uuid, '%s')" % name_or_uuid,
-                                          endpoint=balena.settings.get('pine_endpoint'))['d']
-        for device in devices_by_uuid:
-            device['name'] = device['device_name']
+        # Similar to the partial-name search, the SDK does not have a way to do a partial UUID search. See above for
+        # details.
+        if not have_base_request:
+            devices_by_uuid = [d for d in all_devices if d['uuid'].startswith(name_or_uuid)]
+        else:
+            request = BaseRequest()
+            devices_by_name = request.request('device', 'GET',
+                                              raw_query="$filter=startswith(uuid, '%s')" % name_or_uuid,
+                                              endpoint=balena.settings.get('pine_endpoint'))['d']
 
         if len(devices_by_uuid) == 1:
             device = devices_by_uuid[0]
             if is_name is not None:
                 uuid = device['uuid']
-                name = device['name']
+                name = device['device_name']
                 __logger.debug("Found device %s (%s) by partial UUID match." % (name, uuid))
             else:
-                __logger.debug("Found candidate device %(name)s (%(uuid)s) by partial UUID match." % device)
+                __logger.debug("Found candidate device %(device_name)s (%(uuid)s) by partial UUID match." % device)
         elif len(devices_by_uuid) > 1:
             __logger.warning("Found multiple devices matching partial UUID string:\n    %s" %
-                             "\n    ".join(["%(name)s (%(uuid)s)" % device for device in devices_by_uuid]))
+                             "\n    ".join(["%(device_name)s (%(uuid)s)" % device for device in devices_by_uuid]))
 
     if uuid is None:
         num_devices = len(devices_by_name) + len(devices_by_uuid)
@@ -107,10 +135,10 @@ def get_device_uuid(name_or_uuid, is_name=None, return_name=False, balena=None, 
             raise ValueError("Found multiple devices matching query string.")
         elif len(devices_by_name) == 1:
             uuid = devices_by_name[0]['uuid']
-            name = devices_by_name[0]['name']
+            name = devices_by_name[0]['device_name']
         elif len(devices_by_uuid) == 1:
             uuid = devices_by_uuid[0]['uuid']
-            name = devices_by_uuid[0]['name']
+            name = devices_by_uuid[0]['device_name']
         else:
             raise DeviceNotFound("No device found matching query string.")
 
